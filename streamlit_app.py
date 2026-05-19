2b14e4aa458d505c0f4917502e308762d7e383be
LahThan <fthankrndi@gmail.com>
2026-05-19 11:04:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("#THANKERENBANGET")
 st.write(
-    "Mari bermain dengan gw[docs.streamlit.io](https://docs.streamlit.io/)."
+    "Mari bermain dengan gw [docs.streamlit.io](https://docs.streamlit.io/)."
 )
