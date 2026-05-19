b5b0274aff5c208be542c38cfffa537e22fc4f3e
LahThan <fthankrndi@gmail.com>
2026-05-19 11:27:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,10 @@ a soft return is used for the next line.
 Two (or more) newline characters in a row will result in a hard return.
 '''
 st.markdown(multi)
-sentiment_mapping = ["one", "two", "three", "four", "five"]
+sentiment_mapping = ["satu", "dua", "tiga", "empat", "lima"]
 selected = st.feedback("stars")
 if selected is not None:
-    st.markdown(f"Terimakasih atas {sentiment_mapping[selected]} bintang(s).")
+    st.markdown(f"Terimakasih atas bintang {sentiment_mapping[selected]}.")
 sentiment_mapping = [":material/thumb_down:", ":material/thumb_up:"]
 selected = st.feedback("thumbs")
 if selected is not None:
